d29fcae2f495cf8ba7fd4eafa356fa5050db6d03
ajuggler <ajuggler@users.noreply.github.com>
2025-04-08 17:36:16 -0600

    Example: with signing-key file

--- app/Main.hs
+++ app/Main.hs
@@ -24,7 +24,7 @@ import           GHC.Generics
 import qualified Network.HTTP.Types          as HttpTypes
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Middleware.Cors
-import           PlutusLedgerApi.V3          (fromBuiltin)
+import           PlutusLedgerApi.V3          (fromBuiltin, toBuiltinData)
 import           Prelude
 import           Servant
 import           System.Environment          (getArgs)
@@ -34,8 +34,9 @@ import           Text.Parsec                 (parse)
 
 import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1, Fr)
 import           ZkFold.Base.Protocol.Plonkup.Prover.Secret  (PlonkupProverSecret)
-import           ZkFold.Cardano.OffChain.Utils               (currencySymbolOf)
+import           ZkFold.Cardano.OffChain.Utils               (byteStringAsHex, currencySymbolOf)
 import qualified ZkFold.Cardano.OnChain.BLS12_381.F          as F
+import           ZkFold.Cardano.OnChain.Plonkup.Data         (ProofBytes (..))
 import           ZkPass.Cardano.Example.IdentityCircuit      (identityCircuitVerificationBytes, zkPassResultVerificationBytes)
 import           ZkPass.Cardano.Example.ZkPassResult         (zkPassResult)
 import           ZkPass.Cardano.UPLC.ZkPassToken             (forwardingMintCompiled, zkPassTokenCompiled)
@@ -93,7 +94,7 @@ setupResponse ctx SetupParams{..} InputParams{..} = do
 
   let plutusValidators = [zkPassTokenCompiled setup, forwardingMintCompiled ipTaskId]
       policyId         = show $ currencySymbolOf $ head plutusValidators
-      validators       = GYPlutusScript . validatorFromPlutus @'PlutusV3 <$> plutusValidators
+      validators       = GYPlutusScript . validatorFromPlutus @PlutusV3 <$> plutusValidators
 
   let valTxOuts = GYTxOut (fromAddrHex ipZkpassAddr) mempty Nothing . Just <$> validators
       skeleton  = mconcat $ mustHaveOutput <$> valTxOuts
@@ -114,7 +115,177 @@ setupResponse ctx SetupParams{..} InputParams{..} = do
 handleSetup :: Ctx -> SetupParams -> InputParams -> IO SetupResponse
 handleSetup = setupResponse
 
-------------------------- :Sign & Submit: -------------------------
+------------------------- :Transfer: -------------------------
+
+handleTransfer :: Ctx -> TransferInput -> IO UnsignedTxResponse
+handleTransfer Ctx{..} TransferInput{..} = do
+  let nid       = cfgNetworkId ctxCoreCfg
+      providers = ctxProviders
+
+  let forwardingMintValidator = validatorFromPlutus @PlutusV3 $ forwardingMintCompiled tiTaskId
+      forwardingMintAddr      = addressFromValidator nid forwardingMintValidator
+
+  let cs          = mintingPolicyIdToCurrencySymbol . fromString $ tiPolicyId
+      inlineDatum = Just (datumFromPlutusData cs, GYTxOutUseInlineDatum @PlutusV3)
+
+  params <- gyGetProtocolParameters providers
+  let outMin          = GYTxOut forwardingMintAddr (valueFromLovelace 0) inlineDatum Nothing
+      minUtxoLovelace = toInteger $ minimumUTxO params outMin
+
+  if tiReward >= minUtxoLovelace
+    then do
+      let rewardValue = valueFromLovelace tiReward
+          skeleton    = mustHaveOutput (GYTxOut forwardingMintAddr rewardValue inlineDatum Nothing)
+
+      txBody <- runGYTxBuilderMonadIO nid
+                                      providers
+                                      tiUsedAddrs
+                                      tiChangeAddr
+                                      Nothing
+                                      (buildTxBody skeleton)
+
+      return $ unSignedTxWithFee txBody
+
+    else throwIO $ userError "Reward must be at least minimumUTxO lovelace."
+
+------------------------- :Minting ZkPass Token: -------------------------
+
+-- | Minting input parameters.
+data MintInput = MintInput
+  { miUsedAddrs       :: ![GYAddress]
+  , miChangeAddr      :: !GYAddress
+  , miBeneficiaryAddr :: !String
+  , miScriptsTxOutRef :: !String
+  } deriving stock (Show, Generic)
+    deriving anyclass FromJSON
+
+-- | ZkPass response parameters.
+data ZkPassResponse = ZkPassResponse
+  { zkprResult   :: !String
+  , zkprToken    :: !String
+  , zkprUnsigned :: !UnsignedTxResponse
+  } deriving stock (Show, Generic)
+    deriving anyclass ToJSON
+
+handleMint :: Ctx -> SetupParams -> MintInput -> IO ZkPassResponse
+handleMint Ctx{..} SetupParams{..} MintInput{..} = do
+  let nid       = cfgNetworkId ctxCoreCfg
+      providers = ctxProviders
+
+  case parse parseAddressAny "" miBeneficiaryAddr of
+    Right benAddr -> do
+      zkpr <- zkPassResult
+
+      let (setup, input, proof) = zkPassResultVerificationBytes spX spPS $ F.toInput zkpr
+          zkPassTokenValidator  = validatorFromPlutus @PlutusV3 $ zkPassTokenCompiled setup
+          zkPassPolicyId        = mintingPolicyId zkPassTokenValidator
+          zkPassTokenName       = coerce @AssetName @GYTokenName . AssetName . fromBuiltin $ F.fromInput input
+          zkPassToken           = GYToken zkPassPolicyId zkPassTokenName
+          tokens                = valueMake $ Map.singleton zkPassToken 1
+          redeemer              = redeemerFromPlutusData proof
+
+      let txOutRefSetup = txOutRefFromTuple (fromString miScriptsTxOutRef, 0)
+          refScript     = GYMintReference @PlutusV3 txOutRefSetup zkPassTokenValidator
+          skeleton      = mustHaveOutput (GYTxOut (addressFromApi benAddr) tokens Nothing Nothing)
+                       <> mustMint refScript redeemer zkPassTokenName 1
+
+      txBody <- runGYTxBuilderMonadIO nid
+                                      providers
+                                      miUsedAddrs
+                                      miChangeAddr
+                                      Nothing
+                                      (buildTxBody skeleton)
+
+      return $ ZkPassResponse (byteStringAsHex $ fromBuiltin zkpr) (show zkPassToken) (unSignedTxWithFee txBody)
+
+    Left err      -> throwIO . userError . show $ err
+
+------------------------- :Burning: -------------------------
+
+-- | Burning input parameters.
+data BurnInput = BurnInput
+  { biUsedAddrs       :: ![GYAddress]
+  , biChangeAddr      :: !GYAddress
+  , biTaskId          :: !Integer
+  , biZkPassToken     :: !String
+  , biScriptsTxOutRef :: !String
+  } deriving stock (Show, Generic)
+    deriving anyclass FromJSON
+
+-- | Burning response parameters.
+data BurnResponse = BurnResponse
+  { brTxId :: !String
+  } deriving stock (Show, Generic)
+    deriving anyclass ToJSON
+
+handleBurn :: Ctx -> SetupParams -> BurnInput -> IO BurnResponse
+handleBurn Ctx{..} SetupParams{..} BurnInput{..} = do
+  let nid       = cfgNetworkId ctxCoreCfg
+      providers = ctxProviders
+
+  let forwardingMintValidator = validatorFromPlutus @PlutusV3 $ forwardingMintCompiled biTaskId
+      forwardingMintAddr      = addressFromValidator nid forwardingMintValidator
+
+  let zkpassAsset = fromString biZkPassToken :: GYAssetClass
+
+  case zkpassAsset of
+    GYToken zkpPolicy zkpTokenName -> do
+      let cs          = mintingPolicyIdToCurrencySymbol zkpPolicy
+          inlineDatum = GYOutDatumInline $ datumFromPlutusData cs
+
+      utxos <- runGYTxQueryMonadIO nid
+                                   providers
+                                   (utxosAtAddress forwardingMintAddr (Just GYLovelace))
+
+      let utxosList = utxosToList $ filterUTxOs (\u -> utxoOutDatum u == inlineDatum) utxos
+
+      case utxosList of
+        [utxo] -> do
+          let (setup, _, _)        = identityCircuitVerificationBytes spX spPS
+              zkPassTokenValidator = validatorFromPlutus @PlutusV3 $ zkPassTokenCompiled setup
+
+          let setupTxOutRef   = txOutRefFromTuple (fromString biScriptsTxOutRef, 0)
+              forwardTxOutRef = txOutRefFromTuple (fromString biScriptsTxOutRef, 1)
+
+          let setupRef   = GYBuildPlutusScriptReference @PlutusV3 setupTxOutRef zkPassTokenValidator
+              forwardRef = GYBuildPlutusScriptReference @PlutusV3 forwardTxOutRef forwardingMintValidator
+              forwardWit = GYTxInWitnessScript forwardRef Nothing unitRedeemer
+
+          let dummyRedeemer' = ProofBytes "" "" "" "" "" "" "" "" "" "" "" "" "" 0 0 0 0 0 0 0 0 0 0 0 0 (F.F 0)
+              dummyRedeemer  = redeemerFromPlutusData $ toBuiltinData dummyRedeemer'
+
+          let skeleton = mustHaveInput (GYTxIn @PlutusV3 (utxoRef utxo) forwardWit)
+                         <> mustHaveInput (GYTxIn (fromString "a2c87eebbec6502089b0d0c73b8952ec8309c993feee1649f79483216270a9c0#0") GYTxInWitnessKey)  -- Example
+                         <> mustMint (GYBuildPlutusScript setupRef) dummyRedeemer zkpTokenName (-1)
+
+          -- Example: with signing-key file
+          let skeyFile = "../hello-servant/tests/keys/bob.skey"
+          skey <- readSigningKey @'GYKeyRolePayment skeyFile
+
+          txBody <- runGYTxBuilderMonadIO nid
+                                          providers
+                                          [unsafeAddressFromText $ T.pack "addr_test1vrl6grwjvrtemdchnpnuma6znc64q43a5vdlarvcec4664czwjjzj"]
+                                          (unsafeAddressFromText $ T.pack "addr_test1vrl6grwjvrtemdchnpnuma6znc64q43a5vdlarvcec4664czwjjzj")
+                                          Nothing
+                                          $ do
+            ownAddrs <- ownAddresses
+            let skeleton' = skeleton <> mustHaveOutput (GYTxOut (head ownAddrs) (utxoValue utxo) Nothing Nothing)
+            buildTxBody skeleton'
+
+          tid <- gySubmitTx providers $ signGYTxBody txBody [skey]
+
+          return $ BurnResponse (show tid)
+
+        _ -> throwIO $ userError "No UTxO with expected datum found."
+    _ -> throwIO $ userError "Missing native token specification."
+
+------------------------- :Unsigned response: -------------------------
+
+data UnsignedTxResponse = UnsignedTxResponse
+  { urspTxBodyHex :: !T.Text              -- ^ Unsigned transaction cbor.
+  , urspTxFee     :: !(Maybe Integer)     -- ^ Tx fees.
+  } deriving stock (Show, Generic)
+    deriving anyclass ToJSON
 
 -- | Construct `UnsignedTxResponse` return type for our endpoint given the transaction body.
 unSignedTxWithFee :: GYTxBody -> UnsignedTxResponse
@@ -123,11 +294,7 @@ unSignedTxWithFee txBody = UnsignedTxResponse
   , urspTxFee      = Just $ txBodyFee txBody
   }
 
-data UnsignedTxResponse = UnsignedTxResponse
-  { urspTxBodyHex :: !T.Text              -- ^ Unsigned transaction cbor.
-  , urspTxFee     :: !(Maybe Integer)     -- ^ Tx fees.
-  } deriving stock (Show, Generic)
-    deriving anyclass ToJSON
+------------------------- :Sign & Submit: -------------------------
 
 -- | Submit parameters to add for witness.
 data AddWitAndSubmitParams = AddWitAndSubmitParams
@@ -167,89 +334,6 @@ data TransferInput = TransferInput
   } deriving stock (Show, Generic)
     deriving anyclass FromJSON
 
-------------------------- :Transfer: -------------------------
-
-handleTransfer :: Ctx -> TransferInput -> IO UnsignedTxResponse
-handleTransfer Ctx{..} TransferInput{..} = do
-  let nid       = cfgNetworkId ctxCoreCfg
-      providers = ctxProviders
-
-  let forwardingMintValidator = validatorFromPlutus @'PlutusV3 $ forwardingMintCompiled tiTaskId
-      forwardingMintAddr      = addressFromValidator nid forwardingMintValidator
-
-  let cs          = mintingPolicyIdToCurrencySymbol . fromString $ tiPolicyId
-      inlineDatum = Just (datumFromPlutusData cs, GYTxOutUseInlineDatum @PlutusV3)
-
-  params <- gyGetProtocolParameters providers
-  let outMin          = GYTxOut forwardingMintAddr (valueFromLovelace 0) inlineDatum Nothing
-      minUtxoLovelace = toInteger $ minimumUTxO params outMin
-
-  if tiReward >= minUtxoLovelace
-    then do
-      let rewardValue = valueFromLovelace tiReward
-          skeleton    = mustHaveOutput (GYTxOut forwardingMintAddr rewardValue inlineDatum Nothing)
-
-      txBody <- runGYTxBuilderMonadIO nid
-                                      providers
-                                      tiUsedAddrs
-                                      tiChangeAddr
-                                      Nothing
-                                      (buildTxBody skeleton)
-
-      return $ unSignedTxWithFee txBody
-
-    else throwIO $ userError "Reward must be at least minimumUTxO lovelace."
-
-------------------------- :Minting ZkPass Token: -------------------------
-
--- | Minting input parameters.
-data MintInput = MintInput
-  { miUsedAddrs       :: ![GYAddress]
-  , miChangeAddr      :: !GYAddress
-  , miBeneficiaryAddr :: !String
-  , miTxOutRefScript  :: !String
-  } deriving stock (Show, Generic)
-    deriving anyclass FromJSON
-
--- | ZkPass response parameters.
-data ZkPassResponse = ZkPassResponse
-  { zkprResult   :: !String
-  , zkprUnsigned :: !UnsignedTxResponse
-  } deriving stock (Show, Generic)
-    deriving anyclass ToJSON
-
-handleMint :: Ctx -> SetupParams -> MintInput -> IO ZkPassResponse
-handleMint Ctx{..} SetupParams{..} MintInput{..} = do
-  let nid       = cfgNetworkId ctxCoreCfg
-      providers = ctxProviders
-
-  case parse parseAddressAny "" miBeneficiaryAddr of
-    Right benAddr -> do
-      zkpr <- zkPassResult
-
-      let (setup, input, proof) = zkPassResultVerificationBytes spX spPS $ F.toInput zkpr
-          zkPassTokenValidator  = validatorFromPlutus @'PlutusV3 $ zkPassTokenCompiled setup
-          zkPassTokenName       = coerce @AssetName @GYTokenName . AssetName . fromBuiltin $ F.fromInput input
-          zkPassToken           = GYToken (mintingPolicyId zkPassTokenValidator) zkPassTokenName
-          tokens                = valueMake $ Map.singleton zkPassToken 1
-          redeemer              = redeemerFromPlutusData proof
-
-      let txOutRefSetup = txOutRefFromTuple (fromString miTxOutRefScript, 0)
-          refScript     = GYMintReference @PlutusV3 txOutRefSetup zkPassTokenValidator
-          skeleton      = mustHaveOutput (GYTxOut (addressFromApi benAddr) tokens Nothing Nothing)
-                       <> mustMint refScript redeemer zkPassTokenName 1
-
-      txBody <- runGYTxBuilderMonadIO nid
-                                      providers
-                                      miUsedAddrs
-                                      miChangeAddr
-                                      Nothing
-                                      (buildTxBody skeleton)
-
-      return $ ZkPassResponse (show zkpr) (unSignedTxWithFee txBody)
-
-    Left err      -> throwIO . userError . show $ err
-
 ------------------------- :Server: -------------------------
 
 -- | Type for our Servant API.
@@ -257,8 +341,10 @@ type API = "setup" :> ReqBody '[JSON] InputParams
                    :> Post    '[JSON] SetupResponse
       :<|> "transfer" :> ReqBody '[JSON] TransferInput
                       :> Post    '[JSON] UnsignedTxResponse
-      :<|> "mint"     :> ReqBody '[JSON] MintInput
-                      :> Post    '[JSON] ZkPassResponse
+      :<|> "mint" :> ReqBody '[JSON] MintInput
+                  :> Post    '[JSON] ZkPassResponse
+      :<|> "burn" :> ReqBody '[JSON] BurnInput
+                  :> Post    '[JSON] BurnResponse
       :<|> "add-wit-and-submit" :> ReqBody '[JSON] AddWitAndSubmitParams
                                 :> Post    '[JSON] SubmitTxResponse
 
@@ -267,6 +353,7 @@ server :: Ctx -> SetupParams -> ServerT API IO
 server ctx sp = handleSetup ctx sp
            :<|> handleTransfer ctx
            :<|> handleMint ctx sp
+           :<|> handleBurn ctx sp
            :<|> handleAddWitAndSubmitTx ctx
 
 appApi :: Proxy API
